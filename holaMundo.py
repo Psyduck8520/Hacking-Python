17c77cc85bbd78b54ecaeae60af855f5d7aaea15
Psyduck8520 <exprogresive@gmail.com>
2020-11-01 22:14:30 -0500

    Creacion de archivos para las clases

---
+++ holaMundo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python  """ seleccion de interprete lo que nos ayuda a evitar errores """
+#_*_ coding: utf8 _*_   """ la seleción de la codificación que no ayuda evitar problemas de impresión """   
+print("hola mundo")
+saludo = "hola usuario "
+# print(type(saludo))  para  ver los tipo de dato 
+type('a')
+print(type("hola mundo"))
+type(saludo)
+
+
+
+
+
+
